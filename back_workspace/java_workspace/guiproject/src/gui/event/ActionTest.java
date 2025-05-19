3cae87380f1c8c8db276d820c2098c17d0ad1c5e
devlearncampus <devlearncampus.com>
2025-05-19 13:57:33 +0900

    금일수업

--- back_workspace/java_workspace/guiproject/src/gui/event/ActionTest.java
+++ back_workspace/java_workspace/guiproject/src/gui/event/ActionTest.java
@@ -4,10 +4,44 @@ html에서 클릭이벤트는 click
 java에서는 클릭 이벤트라는 용어 자체가 없다.. action 에 소속
 */
 package gui.event;
+import java.awt.Frame;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.TextField;
+import java.awt.Choice;
 
 class ActionTest {
 	public static void main(String[] args) {
-		Frame frame;
+		Frame frame=null;
+		Button bt=null;
+		TextField t=null;
+		Choice ch=null;//html에서의 select박스
 		
+		frame = new Frame();
+		bt = new Button("click me !");
+		t=new TextField(20);
+		ch=new Choice();
+		
+		ch.addItem("choose your mail server");
+		ch.addItem("@naver.com"); //html 에서의 <option>
+		ch.addItem("@gmail.com");
+		ch.addItem("@daum.net");
+		
+		//js에서처럼 bt.addEventLisetener()메서드를 버튼에 연결하는
+		//과정을 동일하게 진행...
+		bt.addActionListener(new MyActionListener());//이벤트를 구현한 객체의 인스턴스
+		
+		//텍스트필드와 리스너와의 연결
+		t.addKeyListener(new MyKeyListener()); 
+		ch.addItemListener(new MyItemListener());
+		frame.addMouseListener(new MyMouseListener());
+		
+		frame.setLayout(new FlowLayout());
+		frame.add(bt);
+		frame.add(t);
+		frame.add(ch);
+		
+		frame.setSize(300,400);
+		frame.setVisible(true);
 	}
 }
