da500e0a45f5d78626bad9359a9751dffb70f490
devlearncampus <devlearncampus.com>
2025-05-21 10:40:12 +0900

    금일수업

---
+++ front_workspace/js_workspace/미로게임/Hero.js
@@ -0,0 +1,5 @@
+class Hero extends GameObject{
+    constructor(container, x, y, width, height, velX, velY, bg, border, borderColor){
+        super(container, x, y, width, height, velX, velY, bg, border, borderColor);
+    }
+}
